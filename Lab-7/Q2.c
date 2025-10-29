abfaea86807ae5c9fb3a9dd76f2194c994930ddd
jw-albert <albert@mail.jw-albert.tw>
2025-10-29 10:43:42 +0800

    Disable buzzer functionality in Buzz function
    Commented out the buzzer functionality in the Buzz function.

--- Lab-7/Q2.c
+++ Lab-7/Q2.c
@@ -25,12 +25,12 @@
 void Buzz(int number)
 {
 	int i;
-	for (i = 0; i < number; i++) {
-		PB11 = 0; // PB11 = 0 to turn on Buzzer
-		CLK_SysTickDelay(100000);  // Delay
-		PB11 = 1; // PB11 = 1 to turn off Buzzer
-		CLK_SysTickDelay(100000);  // Delay
-	}
+	//for (i = 0; i < number; i++) {
+		//PB11 = 0; // PB11 = 0 to turn on Buzzer
+		//CLK_SysTickDelay(100000);  // Delay
+		//PB11 = 1; // PB11 = 1 to turn off Buzzer
+		//CLK_SysTickDelay(100000);  // Delay
+	//}
 }
 
 // Function to generate two random blocks with minimum distance
@@ -444,6 +444,5 @@ int32_t main (void)
 		}
 	}
 }
-
 
 
