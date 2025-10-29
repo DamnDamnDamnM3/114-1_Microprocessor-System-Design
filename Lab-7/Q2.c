ec9c03681af94d502e0a801784af19e4c2cd627e
Andy <154674108+05zhi@users.noreply.github.com>
2025-10-29 10:53:13 +0800

    Update Q2.c

--- Lab-7/Q2.c
+++ Lab-7/Q2.c
@@ -27,9 +27,9 @@ void Buzz(int number)
 	int i;
 	//for (i = 0; i < number; i++) {
 		//PB11 = 0; // PB11 = 0 to turn on Buzzer
-		//CLK_SysTickDelay(100000);  // Delay
+		//CLK_SysTickDelay(10000);  // Delay
 		//PB11 = 1; // PB11 = 1 to turn off Buzzer
-		//CLK_SysTickDelay(100000);  // Delay
+		//CLK_SysTickDelay(10000);  // Delay
 	//}
 }
 
